ec2dbcfd71c5c1aaeaedce850339d3a8620f1cd0
Armaan Goel <armaangoel@Armaans-MacBook-Air.local>
2022-09-03 15:29:58 -0700

    added worker deployment/service and redis

--- submitter.py
+++ submitter.py
@@ -6,10 +6,10 @@ def example(x, y):
 
 func_pkl = dill.dumps(example)
 
-resp = requests.get("http://127.0.0.1:5001/load", data={'func': func_pkl.decode('latin1')})
+resp = requests.get("http://127.0.0.1:30000/load", data={'func': func_pkl.decode('latin1')})
 print(resp.content)
 
 input_pkl = dill.dumps((5, 45))
 
-resp = requests.get("http://127.0.0.1:5001/run", data={'input': input_pkl.decode('latin1')})
+resp = requests.get("http://127.0.0.1:30000/run", data={'input': input_pkl.decode('latin1')})
 print(resp.content)
--- worker/app.py
+++ worker/app.py
@@ -27,5 +27,5 @@ def run():
     return str(result)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port = 5001, debug=True)
+    app.run(host="0.0.0.0", port = 5000, debug=True)
 
